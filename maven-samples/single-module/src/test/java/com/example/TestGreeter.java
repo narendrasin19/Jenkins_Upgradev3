a226a9c699c1f9f7f8ce494e8c76fc911957be48
Narendra <narendra.sin19@gmail.com>
2024-09-05 20:34:31 +0530

    Update TestGreeter.java
    test failed

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,7 +34,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
+ @Test
+  public void Intentaionale_Failed(){
+  assertTrue(false);
+}
+  
+  
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
